46b88cbe87f5d5ed756de66893c9843d93ed9af2
Sneh Mehta <smehta@colum.edu>
2020-11-09 01:52:49 -0600

    Nav links & Hamburger positioning
    The nav links that are on the overlay are now correctly positioned I believe as we want it according to figma and conversation in the team.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function toggleHamburger() {
 	var element = document.getElementById("navbar");
+	var element2 = document.getElementById("hamburger");
+	//What this if statement below states is that whenever the hamburger gets toggled adjust the width of the nav overlay - if the overlay is at 100% width and open then it needs to go down to 0% and closed.
 	if (element.style.width === "100%") {
 		element.style.width = "0%";
+		element2.style.visibility = "visible";
 	} else {
 		element.style.width = "100%";
+		element2.style.visibility = "hidden";
 	}
-  }
+}
